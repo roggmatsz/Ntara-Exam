d0853f1bd69337253c70bc91ea7c7469223f0799
Rogger Matamoros <roggmatz@gmail.com>
2017-06-28 17:51:40 -0400

    Initial commit. Check for File API, created onChange handler function for input tag.

--- Ntara Exam/wwwroot/js/site.js
+++ Ntara Exam/wwwroot/js/site.js
@@ -1,1 +1,20 @@
 ﻿// Write your Javascript code.
+document.addEventListener("DOMContentLoaded", function(event) {
+    if(!window.File) {
+        //tell user to bugger off
+        window.alert('This page requires the File API. Looks like your browser lacks it =(');
+        
+    }
+});
+
+function handleCsv(files) {
+    csv = files[0];
+    if(csv.type.match('text.csv')) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            var container = document.getElementById('dataDisplay');
+            container.innerText = event.target.result;
+        }
+        reader.readAsText(csv);
+    }
+}
