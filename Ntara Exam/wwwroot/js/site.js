ffc7e42b370aeb1de363de7a5b783697e7548c4d
Rogger Matamoros <roggmatz@gmail.com>
2017-06-29 17:49:46 -0400

    Incorporated jQuery-CSV, saving 2D parsed array into persistent JS variable.

--- Ntara Exam/wwwroot/js/site.js
+++ Ntara Exam/wwwroot/js/site.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if(!window.File) {
         //tell user to bugger off
         window.alert('This page requires the File API. Looks like your browser lacks it =(');
-        
     }
+    var parsedCsv = null;
 });
 
 function handleCsv(files) {
@@ -14,6 +14,7 @@ function handleCsv(files) {
         reader.onload = function(event) {
             var container = document.getElementById('dataDisplay');
             container.innerText = event.target.result;
+            parsedCsv = $.csv.toArrays(event.target.result);
         }
         reader.readAsText(csv);
     }
